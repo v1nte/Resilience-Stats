302da2c04815087e4a7631bd2016613a0f29c3ba
v1nte <ivinte99@gmail.com>
2024-04-24 20:45:59 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import BigTable from "./components/BigTable"
+
+function App(){
+
+  return(
+    <div>
+      <h1> Resilience Clan!</h1>
+      <BigTable />
+    </div>
+  )
+
+}
+
+export default App
---
+++ src/components/BigTable.jsx
@@ -0,0 +1,44 @@
+
+import { useState, useEffect } from 'react'
+import DataTable from "react-data-table-component"
+import { columns, rawData } from "../consts.js"
+
+
+export default function BigTable() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const formatedData = rawData.map(player => {
+      return {
+        "name": player.player.name,
+        "id": player.player.steamid,
+        "hltv": (player.average_stats.hltvRating2).toFixed(3),
+        "kills": player.sums_stats.killCount,
+        "deaths": player.sums_stats.deathCount,
+        "assists": player.sums_stats.assistCount,
+        "KD": (player.sums_stats.killCount/player.sums_stats.deathCount).toFixed(2),
+        "oneK": player.sums_stats.oneKillCount,
+        "twoK": player.sums_stats.twoKillCount,
+        "threeK": player.sums_stats.threeKillCount,
+        "fourK": player.sums_stats.fourKillCount,
+        "fiveK": player.sums_stats.fiveKillCount,
+        "matches": player.matches.killCount.length
+      }
+    })
+
+    setData(formatedData)
+
+  }, [])
+
+  return (
+    <DataTable
+      columns={columns}
+      data={data}
+      pagination
+      striped
+      // theme='dark'
+    />
+  )
+
+}
+
